99c100d03b8be008d0874a140faac54dfdc13d0c
SigmaDev (Σ) <61921483+Danroyal001@users.noreply.github.com>
2026-08-12 11:01:52 +0000

    release: bump version to 1.10.0

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -32,7 +32,7 @@ $ErrorActionPreference = 'Stop'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 $Repo = 'Proofboard-inc/proofboard-cli'
-$PinnedVersion = 'v1.9.4'
+$PinnedVersion = 'v1.10.0'
 $PublicDownloadHost = 'https://proofboard.io'
 $BinaryName = 'proofboard-windows-amd64.exe'
 $SystemInstall = $env:PROOFBOARD_SYSTEM_INSTALL -eq '1'
